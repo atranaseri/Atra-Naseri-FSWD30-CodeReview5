113b81099ff6f20327778afda3187d83a6e92a96
ATRA15 <33833239+atranaseri@users.noreply.github.com>
2017-12-23 16:17:23 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,45 @@
+var click = function(){
+	var =i;
+	for ( i = 0; i<100;i++) {
+	return this.likes+i;
+	}
+ document.getElementById('click');
+
+}
+var John = {
+			name: 'John',
+			surname: 'Doe',
+			favoritePerformers: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
+			age: 25,
+   			myPhoto: "file:///Users/atranaseri/Desktop/Code-Review5/CR5/boy1.800x1000.jpg",	
+			likes: 0   
+			},
+			
+var Jane = {
+			name: 'Jane',
+			surname: 'Doe',
+			favoritePerformers: ["pink",  "justin bebar"],
+			age: 25,
+   			myPhoto: "file:///Users/atranaseri/Desktop/Code-Review5/CR5/girl1.800x1000.jpg",	
+			likes: 0   
+			},
+
+
+var Mexina =  {
+			name: 'Mexina',
+			surname: 'Musterman',
+			favoritePerformers: ["Queen","ACDC","shekira" ],
+			age: 25,
+   			myPhoto: "file:///Users/atranaseri/Desktop/Code-Review5/CR5/girl2.800x1000.jpg",	
+			likes:0  
+			},	
+
+var Max =  {
+			name: 'Max',
+			surname: 'Musterman',
+			favoritePerformers: ["brithney", "Kiss", "marlin manson"],
+			age: 25,
+   			myPhoto: "file:///Users/atranaseri/Desktop/Code-Review5/CR5/boy2.800x1000.jpg",	
+			likes: 0    
+			},
+			
